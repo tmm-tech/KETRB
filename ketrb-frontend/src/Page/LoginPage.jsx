bd8999dae39ccce2178f078d41b03918961ec0a1
tmm-tech <tonymugi074@gmail.com>
2024-08-08 16:58:38 +0300

    KERTB

--- ketrb-frontend/src/Page/LoginPage.jsx
+++ ketrb-frontend/src/Page/LoginPage.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./login.css";
+import { FaArrowLeft } from "react-icons/fa";
 import logo from "../Asset/Logo/ketrbl.jpeg";
 
 function LoginPage() {
   const [isSignUpActive, setIsSignUpActive] = useState(false);
- const [selectedLanguage, setSelectedLanguage] = useState('English');
+  const [selectedLanguage, setSelectedLanguage] = useState("English");
   const handleSignUpClick = () => {
     setIsSignUpActive(true);
   };
@@ -24,36 +25,46 @@ function LoginPage() {
         className={`container ${isSignUpActive ? "right-panel-active" : ""}`}
       >
         <div className="form-container sign-up-container">
-        <div className="language-selector">
-          <select value={selectedLanguage} onChange={handleLanguageChange}>
-            <option value="English">English</option>
-            <option value="Spanish">Spanish</option>
-            <option value="French">French</option>
-          </select>
-        </div>
+          <div className="back-icon">
+            <Link to="/">
+              <FaArrowLeft size={20} />
+            </Link>
+          </div>
+          <div className="language-selector">
+            <select value={selectedLanguage} onChange={handleLanguageChange}>
+              <option value="English">English</option>
+              <option value="Spanish">Spanish</option>
+              <option value="French">French</option>
+            </select>
+          </div>
           <form action="">
             <h1>Create Account</h1>
             <span>use your company email for registration</span>
-            <br/>
+            <br />
             <input type="text" name="name" placeholder="Name" />
             <input type="email" name="email" placeholder="Email" />
             <input type="password" name="password" placeholder="Password" />
-            <br/>
+            <br />
             <button>Sign Up</button>
           </form>
         </div>
         <div className="form-container sign-in-container">
-        <div className="language-selector">
-          <select value={selectedLanguage} onChange={handleLanguageChange}>
-            <option value="English">English</option>
-            <option value="Spanish">Spanish</option>
-            <option value="French">French</option>
-          </select>
-        </div>
+          <div className="back-icon">
+            <Link to="/">
+              <FaArrowLeft size={20} />
+            </Link>
+          </div>
+          <div className="language-selector">
+            <select value={selectedLanguage} onChange={handleLanguageChange}>
+              <option value="English">English</option>
+              <option value="Spanish">Spanish</option>
+              <option value="French">French</option>
+            </select>
+          </div>
           <form action="#">
             <h1>Sign In</h1>
             <span>use your account</span>
-            <br/>
+            <br />
             <input type="email" name="email" placeholder="Company Email" />
             <input type="password" name="password" placeholder="Password" />
             <Link to="#">Forgot Your Password</Link>
