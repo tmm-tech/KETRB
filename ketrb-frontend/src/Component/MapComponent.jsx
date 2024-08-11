cea782d985fc5cd2d9513b1e7ba2b847da0d69c2
tmm-tech <tonymugi074@gmail.com>
2024-08-11 19:14:48 +0300

    KETRB

--- ketrb-frontend/src/Component/MapComponent.jsx
+++ ketrb-frontend/src/Component/MapComponent.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import './MapComponent.css';
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./MapComponent.css";
+// Define the icon for the marker
+const markerIcon = new L.Icon({
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png", // Default Leaflet marker icon
+  iconSize: [25, 41], // Size of the icon
+  iconAnchor: [12, 41], // Point of the icon which will correspond to marker's location
+  popupAnchor: [1, -34], // Point from which the popup should open relative to the iconAnchor
+});
+
 const MapComponent = () => {
-  const position = [ -1.28333, 36.81667 ]; // Coordinates for National Bank Building
+  const position = [-1.28333, 36.81667]; // Coordinates for National Bank Building
 
   return (
     <div className="map-container">
-      <MapContainer center={position} zoom={13} style={{ height: '100%', width: '100%' }}>
+      <MapContainer
+        center={position}
+        zoom={13}
+        style={{ height: "100%", width: "100%" }}
+      >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution=''
+          attribution="© OpenStreetMap contributors"
         />
-        <Marker position={position}>
+        <Marker position={position} icon={markerIcon}>
           <Popup>National Bank Building, Nairobi</Popup>
         </Marker>
       </MapContainer>
