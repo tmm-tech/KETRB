b1a34c216c76003238d73b2d58d85bd1c54a45fa
tmm-tech <tonymugi074@gmail.com>
2024-08-11 19:00:07 +0300

    KETRB

--- ketrb-frontend/src/Component/MapComponent.jsx
+++ ketrb-frontend/src/Component/MapComponent.jsx
@@ -1,26 +1,22 @@
-// src/MapComponent.js
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS
-
+import 'leaflet/dist/leaflet.css';
+import './MapComponent.css';
 const MapComponent = () => {
-  // Coordinates for National Bank Building, Harambee Avenue, Nairobi
-  const position = [-1.285280, 36.823650];
+  const position = [ -1.28333, 36.81667 ]; // Coordinates for National Bank Building
 
   return (
-    <MapContainer center={position} zoom={15} style={{ height: "100vh", width: "100%" }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={position}>
-        <Popup>
-          National Bank Building<br />
-          11th Floor, Harambee Avenue<br />
-          Nairobi, Kenya
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div className="map-container">
+      <MapContainer center={position} zoom={13} style={{ height: '100%', width: '100%' }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='© OpenStreetMap contributors'
+        />
+        <Marker position={position}>
+          <Popup>National Bank Building, Nairobi</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 };
 
