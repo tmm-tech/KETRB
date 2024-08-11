a4336ca7120d777336ab16a4d88cbb266f2ca2a8
tmm-tech <tonymugi074@gmail.com>
2024-08-11 17:09:46 +0300

    KETRB

--- ketrb-frontend/src/Page/About.jsx
+++ ketrb-frontend/src/Page/About.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Image1 from "../Asset/IMG_0289.JPG";
-import Image2 from "../Asset/IMG_0988.JPG";
+import Image2 from "../Asset/IMG_0723.JPG";
 import "./about.css";
 import TopBar from "../Component/Topbar";
 import Footer from "../Component/Footer";
