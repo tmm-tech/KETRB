4805171de67c4bf007b87351abd5bf79c1b4a5ae
tmm-tech <tonymugi074@gmail.com>
2024-08-13 23:39:23 +0300

    KETRB

--- ketrb-frontend/src/Component/MobileMenu.jsx
+++ ketrb-frontend/src/Component/MobileMenu.jsx
@@ -12,10 +12,17 @@ const MobileMenu = ({ showMenu, toggleMenu }) => {
       <nav className="mobile-nav-links">
         <Link to="/" className="mobile-nav-link" onClick={toggleMenu}>Home</Link>
         <Link to="/about" className="mobile-nav-link" onClick={toggleMenu}>About Us</Link>
-        <Link to="/contact" className="mobile-nav-link" onClick={toggleMenu}>Contact Us</Link>
+        <Link to="/our-mandate" className="mobile-nav-link" onClick={toggleMenu}>Our Mandate</Link>
+        <Link to="/leadership" className="mobile-nav-link" onClick={toggleMenu}>Leadership</Link>
         <Link to="/news&events" className="mobile-nav-link" onClick={toggleMenu}>News & Events</Link>
+        <Link to="/programs" className="mobile-nav-link" onClick={toggleMenu}>Programs</Link>
+        <Link to="/gallery" className="mobile-nav-link" onClick={toggleMenu}>Gallery</Link>
+        <Link to="/charter" className="mobile-nav-link" onClick={toggleMenu}>Charter</Link>
+        <Link to="/act" className="mobile-nav-link" onClick={toggleMenu}>Act</Link>
         <Link to="/faq" className="mobile-nav-link" onClick={toggleMenu}>FAQ</Link>
+        <Link to="/contact" className="mobile-nav-link" onClick={toggleMenu}>Contact Us</Link>
         <Link to="/login" className="mobile-nav-link" onClick={toggleMenu}>Login</Link>
+
       </nav>
     </div>
   );
