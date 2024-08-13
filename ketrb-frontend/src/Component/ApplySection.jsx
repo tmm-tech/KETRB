80d350e10f077f9133bff599a103d2fa065939e9
tmm-tech <tonymugi074@gmail.com>
2024-08-13 23:25:20 +0300

    KETRB

--- ketrb-frontend/src/Component/ApplySection.jsx
+++ ketrb-frontend/src/Component/ApplySection.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Assuming you're using React Router
-import './applysection.css'; // Import the CSS file for styling
+import { Link } from 'react-router-dom'; 
+import './applysection.css'; 
 
 const ApplySection = () => {
   return (
     <div className="apply-section-container">
       <section className="apply-section">
-        <h1>Join KETRB</h1>
+        <h1>Join The KETRB</h1>
         <p>
           At the Kenya Engineering Technology Registration Board (KETRB), we're committed to advancing engineering technology in Kenya. We’re always on the lookout for talented individuals who are passionate about our mission. If you're eager to contribute and grow with us, we'd love to hear from you.
         </p>
