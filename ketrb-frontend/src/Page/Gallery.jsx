8487085a8d6e0d9a34e44ce91278757816371b3e
tmm-tech <tonymugi074@gmail.com>
2024-08-13 23:50:15 +0300

    KETRB

--- ketrb-frontend/src/Page/Gallery.jsx
+++ ketrb-frontend/src/Page/Gallery.jsx
@@ -8,7 +8,7 @@ import Loading from "../Component/Loading";
 const importAll = (requireContext) => 
   requireContext.keys().map(requireContext);
 
-const images = importAll(require.context('../Asset/Gallery', false, /\.(png|jpe?g|svg|jpg)$/));
+const images = importAll(require.context('../Asset/Gallery', false, /\.(png|jpe?g|svg|JPG)$/));
 
 const Gallery = () => {
   const [selectedImage, setSelectedImage] = useState(null);
