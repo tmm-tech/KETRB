b20489d59aa6d468820b762ea2d679e949223f1f
tmm-tech <tonymugi074@gmail.com>
2024-08-14 20:36:39 +0300

    KERTB

--- ketrb-frontend/src/Component/MapComponent.jsx
+++ ketrb-frontend/src/Component/MapComponent.jsx
@@ -23,10 +23,6 @@ const MapComponent = () => {
         zoom={13}
         style={{ height: "100%", width: "100%" }}
       >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="© OpenStreetMap contributors"
-        />
         <Marker position={position} icon={markerIcon}>
           <Popup>National Bank Building, Nairobi</Popup>
         </Marker>
