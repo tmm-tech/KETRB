3ec07c747bbd610f0df1d8a06fb8519303a49ef3
tmm-tech <tonymugi074@gmail.com>
2024-08-15 22:19:46 +0300

    KETRB

--- ketrb-frontend/src/Page/Act.jsx
+++ ketrb-frontend/src/Page/Act.jsx
@@ -1,18 +1,39 @@
-import React from 'react';
-import './act.css'; 
+import React, { useState, useEffect } from "react";
+import "./act.css";
+import TopBar from "../Component/Topbar";
+import Footer from "../Component/Footer";
+import Loading from "../Component/Loading";
 
 const ActPage = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadData = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      setLoading(false);
+    };
+
+    loadData();
+  }, []);
+
+  if (loading) {
+    return <Loading/>;
+  }
   return (
-    <div className="act-page-container">
-      <h1 className="act-page-title">The Act Document</h1>
-      <div className="pdf-container">
-        <iframe
-          src="/Engineering_Act.pdf"
-          title="Act Document"
-          className="pdf-viewer"
-        />
+    <>
+      <TopBar />
+      <div className="act-page-container">
+        <h1 className="act-page-title">KETRB ACT</h1>
+        <div className="pdf-container">
+          <iframe
+            src="/Engineering_Act.pdf"
+            title="Act Document"
+            className="pdf-viewer"
+          />
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
