772fdcba32ef34703769bfbb033dcc6b80104b1c
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-08-15 23:44:09 +0300

    KETRB

--- ketrb-frontend/src/Page/News.jsx
+++ ketrb-frontend/src/Page/News.jsx
@@ -5,7 +5,7 @@ import Footer from "../Component/Footer";
 import Loading from "../Component/Loading";
 import news1 from "../Asset/News/IMG_3467.jpeg";
 import news2 from "../Asset/News/IMG_3468.jpeg";
-import news3 from "../Asset/Carousel/img_001.jpeg";
+import news3 from "../Asset/Carousel/city.jpeg";
 import news4 from "../Asset/Carousel/lady2.jpeg";
 import news5 from "../Asset/Carousel/man.jpeg";
 import news6 from "../Asset/Carousel/lady&man.jpg";
