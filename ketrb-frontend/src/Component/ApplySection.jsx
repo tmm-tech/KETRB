3054d33a905d703ccfd708b72a22a535b82d84b9
GideonKipkemoi <rotichgideon760@gmail.com>
2024-08-15 08:05:37 +0300

    added target to Link

--- ketrb-frontend/src/Component/ApplySection.jsx
+++ ketrb-frontend/src/Component/ApplySection.jsx
@@ -13,7 +13,7 @@ const ApplySection = () => {
         <p>
           Ready to take the next step in your career and join a dynamic team? Follow our streamlined application process to become a registered member and start making an impact.
         </p>
-        <Link to="https://portal.ketrb.go.ke:81" className="apply-button">
+        <Link to="https://portal.ketrb.go.ke:81" className="apply-button" target='_blank'>
           Apply Now
         </Link>
       </section>
