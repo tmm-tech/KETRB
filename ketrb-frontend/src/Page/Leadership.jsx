c3e80a7064ad33b231adb6d629dc7d1574686ea9
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-08-26 16:40:18 +0300

    KETRB

--- ketrb-frontend/src/Page/Leadership.jsx
+++ ketrb-frontend/src/Page/Leadership.jsx
@@ -11,55 +11,48 @@ import leader5 from "../Asset/Leaders/peter_nganga_kariuki.jpeg";
 import leader6 from "../Asset/Leaders/samuel_muthondu.jpeg";
 import leader7 from "../Asset/Leaders/temoet.jpeg";
 import leader8 from "../Asset/Leaders/THUMBI.png";
+
 const leadershipData = [
   {
     image: leader7,
     name: "MR NICHOLAS KIPRUTO TEMOET",
-    description:
-      "BOARD CHAIRMAN",
+    description: "BOARD CHAIRMAN",
   },
   {
     image: leader1,
     name: "MS ALICE MUTAI",
-    description:
-      "CEO",
+    description: "CEO",
   },
   {
     image: leader4,
     name: "MR FRED NYAMBENE OANDA",
-    description:
-      "BOARD MEMBER",
+    description: "BOARD MEMBER",
   },
   {
     image: leader5,
     name: "MR PETER NG'ANG'A KARIUKI",
-    description:
-      "BOARD MEMBER",
+    description: "BOARD MEMBER",
   },
   {
     image: leader3,
     name: "MRS LINDA AYUKU WEKHOBA",
-    description:
-      "BOARD MEMBER",
+    description: "BOARD MEMBER",
   },
   {
     image: leader8,
     name: "PROF GEORGE MITHURI GITHAMBI",
-    description:
-      "BOARD MEMBER",
+    description: "BOARD MEMBER",
   },
   {
     image: leader2,
     name: "MR CHARLES CHORE MAJANI",
-    description:
-      "BOARD MEMBER",
+    description: "BOARD MEMBER",
   },
   {
     image: leader6,
-    name: "Samuel Muthondu",
-    description:
-      "BOARD MEMBER",
-  }
+    name: "MR SAMUEL MUTHONDU",
+    description: "BOARD MEMBER",
+  },
 ];
 
 const Leadership = () => {
@@ -77,23 +70,57 @@ const Leadership = () => {
   if (loading) {
     return <Loading />;
   }
+
   return (
     <>
       <TopBar />
-      <div style={{ paddingTop: '170px' }}  className="leadership">
+      <div style={{ paddingTop: "170px" }} className="leadership">
         <h1 className="leadership-title">Our Leadership Team</h1>
         <div className="leadership-container">
-          {leadershipData.map((leader, index) => (
-            <div key={index} className="leadership-card">
+          <div className="leadership-hierarchy-line"></div>
+
+          {/* Chairman Row */}
+          <div className="leadership-row chairman">
+            <div className="leadership-card">
               <img
-                src={leader.image}
-                alt={leader.name}
+                src={leader7}
+                alt="MR NICHOLAS KIPRUTO TEMOET"
                 className="leadership-image"
               />
-              <h2 className="leadership-name">{leader.name}</h2>
-              <p className="leadership-description">{leader.description}</p>
+              <h2 className="leadership-name">MR NICHOLAS KIPRUTO TEMOET</h2>
+              <p className="leadership-description">BOARD CHAIRMAN</p>
             </div>
-          ))}
+          </div>
+
+          {/* Members Row */}
+          <div className="leadership-row members">
+            {leadershipData.slice(2, 8).map((leader, index) => (
+              <div key={index} className="leadership-card">
+                <img
+                  src={leader.image}
+                  alt={leader.name}
+                  className="leadership-image"
+                />
+                <h2 className="leadership-name">{leader.name}</h2>
+                <p className="leadership-description">
+                  {leader.description}
+                </p>
+              </div>
+            ))}
+          </div>
+
+          {/* CEO Row */}
+          <div className="leadership-row ceo">
+            <div className="leadership-card">
+              <img
+                src={leader1}
+                alt="MS ALICE MUTAI"
+                className="leadership-image"
+              />
+              <h2 className="leadership-name">MS ALICE MUTAI</h2>
+              <p className="leadership-description">CEO</p>
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
