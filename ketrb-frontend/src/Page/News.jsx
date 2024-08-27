48741f04b9d8d235547e8325f3afde380fdbed05
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-08-27 18:38:51 +0000

    KETRB

--- ketrb-frontend/src/Page/News.jsx
+++ ketrb-frontend/src/Page/News.jsx
@@ -9,7 +9,7 @@ const News = ({ articles }) => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    
+
     const loadData = async () => {
 
       await new Promise((resolve) => setTimeout(resolve, 1000));
@@ -20,10 +20,17 @@ const News = ({ articles }) => {
   }, []);
 
   if (loading) {
-    return <Loading />; 
+    return <Loading />;
   }
 
-const sortedArticles = articles.sort((a, b) => new Date(b.date) - new Date(a.date));
+  const truncateDescription = (description, length) => {
+    if (description.length > length) {
+      return description.substring(0, length) + "...";
+    }
+    return description;
+  };
+
+  const sortedArticles = articles.sort((a, b) => new Date(b.date) - new Date(a.date));
 
   return (
     <>
@@ -37,7 +44,7 @@ const sortedArticles = articles.sort((a, b) => new Date(b.date) - new Date(a.dat
               <div className="news-content">
                 <h3 className="news-item-title">{article.title}</h3>
                 <p className="news-date">Published on: {new Date(article.date).toLocaleDateString()}</p>
-                <p className="news-description">{article.description}</p>
+                <p className="news-description"> {truncateDescription(article.description, 100)}</p>
                 <Link to={`/news&events/${index}`} className="news-link">Read more</Link>
               </div>
             </div>
--- ketrb-frontend/src/Page/NewsDetails.jsx
+++ ketrb-frontend/src/Page/NewsDetails.jsx
@@ -30,7 +30,7 @@ const NewsDetail = ({ articles }) => {
   return (
     <>
       <TopBar />
-      <div className="news-detail-container">
+      <div style={{ paddingTop: '170px' }} className="news-detail-container">
         <h2 className="news-detail-title">{article.title}</h2>
         <img src={article.image} alt={article.title} className="news-detail-image" />
         <p className="news-detail-date">Published on: {new Date(article.date).toLocaleDateString()}</p>
