a0b3936444bded0d9f1831d8a227ddefbe2597ec
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-09-19 21:01:55 +0300

    Update Topbar.jsx

--- ketrb-frontend/src/Component/Topbar.jsx
+++ ketrb-frontend/src/Component/Topbar.jsx
@@ -18,9 +18,64 @@ const TopBar = () => {
   };
   return (
     <header className="top-bar">
+      {/* Toolbar Area */}
+      <div className="toolbar-area bg-gray-800 text-white py-2">
+        <div className="container flex justify-between items-center">
+          <div className="w-full lg:w-1/2 flex justify-start">
+            <ul className="flex items-center flex-wrap menu-lists">
+              <li className="mr-4 flex items-center">
+                <FaEnvelope className="mr-2" />
+                <Link to="mailto:kipkogeit@gmail.com">kipkogeit@gmail.com</Link>
+              </li>
+              <li className="mr-4 flex items-center">
+                <FaPhone className="mr-2" />
+                <Link to="tel:+254729777711">+254 724027435</Link>
+              </li>
+              <li className="mr-4 flex items-center">
+                <FaMapMarker className="mr-2" />
+                <p>Lower Kabete / Kiplombe Eldoret</p>
+              </li>
+            </ul>
+          </div>
+          <div cl   vmassName="w-full lg:w-1/2 flex justify-end">
+            <ul className="flex items-center flex-wrap menu-lists">
+              <li className="mr-4 flex items-center">
+                <FaClock className="mr-2" />
+                <p>Mon - Fri: 8:00 - 16:00, Sat: 8:00 - Noon</p>
+              </li>
+              <li className="mr-4 flex items-center">
+                <Link to="https://web.facebook.com/profile.php?id=61555197753208" target="_blank">
+                  <FaFacebook />
+                </Link>
+              </li>
+              <li className="mr-4 flex items-center">
+                <Link to="#" target="_blank">
+                  <FaTwitter />
+                </Link>
+              </li>
+              <li className="mr-4 flex items-center">
+                <Link to="#" target="_blank">
+                  <FaLinkedin />
+                </Link>
+              </li>
+              <li className="mr-4 flex items-center">
+                <Link to="#" target="_blank">
+                  <FaInstagram />
+                </Link>
+              </li>
+              <li className="mr-4 flex items-center">
+                <Link to="#" target="_blank">
+                  <FaYoutube />
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>   vm
       <div className=".logo-container">
         <Link to="/">
           <img src={logo} alt="logo" className="logo-img" />
+          <h1 className="company-name text-xl font-bold">KENYA ENGINEERING TECHNOLOGY REGISTRATION BOARD</h1>
         </Link>
       </div>
       <nav className={`nav-links ${showMenu ? "hidden" : ""}`}>
