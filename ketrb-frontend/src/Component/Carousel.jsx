7c23c40ae28a823b8847130bf8463a4821517ff9
Tony Mwangi Mugi <tonymugi074@gmail.com>
2024-09-30 18:27:49 +0300

    KETRB

--- ketrb-frontend/src/Component/Carousel.jsx
+++ ketrb-frontend/src/Component/Carousel.jsx
@@ -5,42 +5,49 @@ const images = [
   {
     className: "image0",
     coreValueTitle: "KENYA ENGINEERING TECHNOLOGY REGISTRATION BOARD",
-    coreValueContent:
-      "",
+    coreValueContent: "",
+    url: '../Asset/Gallery/IMG_0093.JPG',
   },
   {
     className: "image1",
     coreValueTitle: "Accountability",
-    coreValueContent:
-      "We take responsibility for our actions and decisions, ensuring transparency and trust.",
+    coreValueContent: "We take responsibility for our actions and decisions.",
+    url: '../Asset/Gallery/IMG_0723.JPG',
   },
   {
     className: "image2",
     coreValueTitle: "Teamwork",
-    coreValueContent:
-      "We collaborate and support each other to achieve common goals.",
+    coreValueContent: "We collaborate and support each other to achieve common goals.",
+    url: '../Asset/Carousel/lady&man.jpg',
   },
   {
     className: "image3",
     coreValueTitle: "Integrity",
-    coreValueContent:
-      "We adhere to the highest ethical standards, demonstrating honesty and fairness in every action.",
+    coreValueContent: "We adhere to the highest ethical standards.",
+    url: '../Asset/Gallery/IMG_3465.jpeg',
   },
   {
     className: "image4",
     coreValueTitle: "Innovation",
-    coreValueContent:
-      "We foster a culture of creativity and continuous improvement, embracing new ideas and technologies.",
+    coreValueContent: "We foster a culture of creativity.",
+    url: '../Asset/Gallery/IMG_0136.JPG',
   },
 ];
 
 function Carousel() {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [loadedImages, setLoadedImages] = useState({});
+
+  // Preload the first image for faster first render
+  useEffect(() => {
+    const img = new Image();
+    img.src = images[currentSlide].url;
+  }, [currentSlide]);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentSlide((prevSlide) => (prevSlide + 1) % images.length);
-    }, 5000); // Change slide every 5 seconds
+    }, 5000);
 
     return () => clearInterval(interval);
   }, []);
@@ -55,6 +62,13 @@ function Carousel() {
     );
   };
 
+  const handleImageLoad = (index) => {
+    setLoadedImages((prevLoadedImages) => ({
+      ...prevLoadedImages,
+      [index]: true,
+    }));
+  };
+
   return (
     <section style={{ paddingTop: '130px', position: 'relative', overflow: 'hidden' }} className="u-section-1" id="sec-63bc">
       <div className="u-carousel u-expanded-width u-slider-1">
@@ -64,12 +78,18 @@ function Carousel() {
         >
           {images.map((image, index) => (
             <div
-              className={`u-carousel-item ${
-                currentSlide === index ? "u-active" : ""
-              }`}
+              className={`u-carousel-item ${currentSlide === index ? "u-active" : ""}`}
               key={index}
             >
               <div className={`u-image u-shading ${image.className}`}>
+                {!loadedImages[index] && <span>Loading...</span>}
+                <img
+                  src={image.url}
+                  alt={image.coreValueTitle}
+                  className="carousel-image"
+                  loading="lazy"
+                  onLoad={() => handleImageLoad(index)}
+                />
                 <div className="u-container-layout">
                   <div className="u-core-values">
                     <h2>{image.coreValueTitle}</h2>
