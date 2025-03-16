b15bfa226847807baf0f3a64f22e735097f4ba2f
tmm-tech <tonymugi074@gmail.com>
2025-03-16 19:50:47 +0300

    landing page

--- ketrb-frontend/src/Component/Carousel.jsx
+++ ketrb-frontend/src/Component/Carousel.jsx
@@ -52,7 +52,7 @@ function Carousel() {
       );
     }, 5000);
     return () => clearInterval(interval);
-  }, [carouselItems.length]);
+  }, [carouselItems]);
 
   const nextSlide = () => {
     setCurrentSlide((prev) =>
