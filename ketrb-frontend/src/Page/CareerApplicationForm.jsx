0b871e96fe1024bb8563ca6196c68b182d7f7647
tmmtech <tonymugi074@gmail.com>
2025-04-06 01:10:42 +0300

    Application

--- ketrb-frontend/src/Page/CareerApplicationForm.jsx
+++ ketrb-frontend/src/Page/CareerApplicationForm.jsx
@@ -266,14 +266,13 @@ const CareerApplicationForm = () => {
                   First Name <span className="text-red-500">*</span>
                 </Label>
                 <div className="relative">
-                  <User className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 h-5 w-5" />
+                  <User className="absolute left-3 right-6 top-1/2 -translate-y-1/2 text-gray-400 h-5 w-5" />
                   <Input
                     id="first_name"
                     name="first_name"
                     value={formData.first_name}
                     onChange={handleChange}
                     required
-                    className="left-10"
                   />
                 </div>
               </div>
